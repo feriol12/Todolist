468756f6b065b5f84e3ccb7afe75f3dc701018ca
Dimas <othentiquedimas@gmail.com>
2025-11-11 12:02:15 +0100

    Ajout de la fonctionnalité project et mise à jour général dash

---
+++ front/assets/js/modal-project.js
@@ -0,0 +1,6 @@
+// À ajouter dans ton code existant
+document.getElementById('saveProject')?.addEventListener('click', handleProjectSave);
+
+async function handleProjectSave() {
+    // Logique de sauvegarde du projet
+}
